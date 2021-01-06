6c5969d75fb9ad61e92ad9f1eaf15970d3a8e602
Muhammed Uluyol <uluyol0@gmail.com>
2021-01-06 12:45:32 -0500

    proto: add FlowMarker equality checker

--- heyp/proto/alg.cc
+++ heyp/proto/alg.cc
@@ -7,10 +7,41 @@
 
 namespace heyp {
 
+#define SAME_OR_RETURN(field_name)            \
+  if (lhs.field_name() != rhs.field_name()) { \
+    return false;                             \
+  }
+
+bool IsSameFlow(const proto::FlowMarker& lhs, const proto::FlowMarker& rhs,
+                CompareFlowOptions options) {
+  if (options.cmp_fg) {
+    SAME_OR_RETURN(src_dc);
+    SAME_OR_RETURN(dst_dc);
+  }
+  if (options.cmp_src_host) {
+    SAME_OR_RETURN(src_addr);
+  }
+  if (options.cmp_host_flow) {
+    SAME_OR_RETURN(dst_addr);
+    SAME_OR_RETURN(protocol);
+    SAME_OR_RETURN(src_port);
+    SAME_OR_RETURN(dst_port);
+  }
+  if (options.cmp_host_unique_id) {
+    SAME_OR_RETURN(host_unique_id);
+  }
+  return true;
+}
+
 size_t HashHostFlowNoId::operator()(const proto::FlowMarker& marker) const {
   return absl::Hash<std::tuple<absl::string_view, int32_t, int32_t, int32_t>>()(
       {marker.dst_addr(), marker.protocol(), marker.src_port(),
        marker.dst_port()});
+}
+
+bool EqHostFlowNoId::operator()(const proto::FlowMarker& lhs,
+                                const proto::FlowMarker& rhs) const {
+  return IsSameFlow(lhs, rhs, {.cmp_fg = false, .cmp_host_unique_id = false});
 }
 
 }  // namespace heyp
--- heyp/proto/alg.h
+++ heyp/proto/alg.h
@@ -5,6 +5,27 @@
 
 namespace heyp {
 
+// CompareFlowOptions restricts the set of fields to compare when comparing
+// FlowMarkers.
+struct CompareFlowOptions {
+  // Include FlowGroup fields in comparison (src_dc, dst_dc)
+  bool cmp_fg = true;
+
+  // Include source host address in comparison (src_addr)
+  bool cmp_src_host = true;
+
+  // Include host-level flow information in comparison
+  // (dst_addr, protocol, src_port, dst_port).
+  bool cmp_host_flow = true;
+
+  // Include source host's unique identifier for the flow in the comparison
+  // (host_unique_id).
+  bool cmp_host_unique_id = true;
+};
+
+bool IsSameFlow(const proto::FlowMarker& lhs, const proto::FlowMarker& rhs,
+                CompareFlowOptions options = {});
+
 struct HashHostFlowNoId {
   size_t operator()(const proto::FlowMarker& marker) const;
 };
