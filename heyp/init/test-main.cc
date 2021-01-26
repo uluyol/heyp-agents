2397e2095b6b7b68f91594b47141fca120014884
Muhammed Uluyol <uluyol0@gmail.com>
2021-01-26 13:41:56 -0500

    init: always log to stderr in tests

--- heyp/init/test-main.cc
+++ heyp/init/test-main.cc
@@ -1,8 +1,10 @@
+#include "glog/logging.h"
 #include "gtest/gtest.h"
 #include "heyp/init/init.h"
 
 int main(int argc, char **argv) {
   testing::InitGoogleTest(&argc, argv);
   heyp::MainInit(&argc, &argv);
+  FLAGS_logtostderr = 1;
   return RUN_ALL_TESTS();
 }
