818d7dfd69ffff5445e46bdf86b9762485dc341e
Muhammed Uluyol <uluyol0@gmail.com>
2021-01-27 11:42:27 -0500

    proto: use unordered equals in AllocBundleEq

--- heyp/proto/testing.h
+++ heyp/proto/testing.h
@@ -11,16 +11,30 @@ MATCHER_P(AllocBundleEq, other, "") {
   if (arg.flow_allocs_size() != other.flow_allocs_size()) {
     return false;
   }
-  for (int i = 0; i < arg.flow_allocs_size(); i++) {
+  std::vector<int> remaining_indicies;
+  remaining_indicies.reserve(arg.flow_allocs_size());
+  for (int i = 0; i < arg.flow_allocs_size(); ++i) {
+    remaining_indicies.push_back(i);
+  }
+  for (int i = 0; i < arg.flow_allocs_size(); ++i) {
     const proto::FlowAlloc& a = arg.flow_allocs(i);
-    const proto::FlowAlloc& b = other.flow_allocs(i);
-    if (!IsSameFlow(a.flow(), b.flow())) {
-      return false;
+    bool found_match = false;
+    for (int ji = 0; ji < remaining_indicies.size(); ++ji) {
+      int j = remaining_indicies[ji];
+
+      const proto::FlowAlloc& b = other.flow_allocs(j);
+      bool is_match = IsSameFlow(a.flow(), b.flow()) &&
+                      (a.hipri_rate_limit_bps() == b.hipri_rate_limit_bps()) &&
+                      (a.lopri_rate_limit_bps() == b.lopri_rate_limit_bps());
+
+      if (is_match) {
+        found_match = true;
+        remaining_indicies[ji] = remaining_indicies.back();
+        remaining_indicies.pop_back();
+        break;
+      }
     }
-    if (a.hipri_rate_limit_bps() != b.hipri_rate_limit_bps()) {
-      return false;
-    }
-    if (a.lopri_rate_limit_bps() != b.lopri_rate_limit_bps()) {
+    if (!found_match) {
       return false;
     }
   }
