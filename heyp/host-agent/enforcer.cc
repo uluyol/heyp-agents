abf1d2d5cc0a21dcfb05064896b8307285f5dd7d
Muhammed Uluyol <uluyol0@gmail.com>
2021-02-06 14:52:44 -0500

    host-agent: add enforcer TODOs

--- heyp/host-agent/enforcer.cc
+++ heyp/host-agent/enforcer.cc
@@ -4,6 +4,11 @@
 
 namespace heyp {
 
+// TODO: track state
+// TODO: translate allocs to rules
+// TODO: translate rules to commands
+// TODO: commands to delete old rules
+
 void HostEnforcer::EnforceAllocs(const FlowStateProvider& flow_state_provider,
                                  const proto::AllocBundle& bundle) {
   LOG(INFO) << "TODO: implement alloc enforcement";
