8db860a408808cc49b37e639ddf3ee9efe9d7b33
Muhammed Uluyol <uluyol@umich.edu>
2021-02-23 11:44:29 -0500

    host-agent/linux-enforcer: fix test after json change

--- heyp/host-agent/linux-enforcer/tc-caller-test.cc
+++ heyp/host-agent/linux-enforcer/tc-caller-test.cc
@@ -8,7 +8,7 @@ namespace {
 
 TEST(TcCallerTest, YieldsExpectedResult) {
   TcCaller caller("heyp/host-agent/linux-enforcer/fake-tc-for-test");
-  EXPECT_THAT(caller.Call({"-j", "qdisc", "list"}),
+  EXPECT_THAT(caller.Call({"-j", "qdisc", "list"}, true),
               testing::Property(&absl::Status::ok, testing::IsTrue()));
   EXPECT_EQ(caller.GetResult()->at(0)["dev"].get_string().value(), "lo");
   EXPECT_EQ(caller.GetResult()->at(1)["dev"].get_string().value(), "ens33");
