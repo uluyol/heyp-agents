41c244b6f8a0c7c79dc311496369bf2e69957c89
Muhammed Uluyol <uluyol@umich.edu>
2021-03-19 11:19:34 -0400

    cmd/deploy-heyp: clean up a bit

--- go/cmd/deploy-heyp/main.go
+++ go/cmd/deploy-heyp/main.go
@@ -10,41 +10,22 @@ import (
 	"github.com/uluyol/heyp-agents/go/deploy/actions"
 )
 
-type cmd struct {
-	name                string
-	shortDesc, longDesc string
-	exec                func(fs *flag.FlagSet)
-}
-
-func (c *cmd) Name() string     { return c.name }
-func (c *cmd) Synopsis() string { return c.shortDesc }
-func (c *cmd) Usage() string {
-	if c.longDesc == "" {
-		return c.shortDesc
-	}
-	return c.longDesc
-}
-func (c *cmd) SetFlags(fs *flag.FlagSet) {}
-func (c *cmd) Execute(ctx context.Context, fs *flag.FlagSet,
-	args ...interface{}) subcommands.ExitStatus {
-
-	c.exec(fs)
-	return subcommands.ExitSuccess
-}
-
-type mkBundleCmdState struct {
-	cmd
+type mkBundleCmd struct {
 	binDir      string
 	tarballPath string
 }
 
-func (c *mkBundleCmdState) SetFlags(fs *flag.FlagSet) {
-	fs.StringVar(&c.binDir, "bin", "./bazel-bin", "path to output binaries")
-	fs.StringVar(&c.tarballPath, "out", "heyp-bundle.tar.xz",
-		"path to xz'd output tarball")
+func (c *mkBundleCmd) Name() string     { return "mk-bundle" }
+func (c *mkBundleCmd) Synopsis() string { return "bundle experiment code into a tarball" }
+func (c *mkBundleCmd) Usage() string    { return "" }
+
+func (c *mkBundleCmd) SetFlags(fs *flag.FlagSet) {
+	fs.StringVar(&c.binDir, "bin", "./bazel-bin",
+		"path to output binaries")
+	bundleVar(&c.tarballPath, fs)
 }
 
-func (c *mkBundleCmdState) Execute(ctx context.Context, fs *flag.FlagSet,
+func (c *mkBundleCmd) Execute(ctx context.Context, fs *flag.FlagSet,
 	args ...interface{}) subcommands.ExitStatus {
 
 	err := actions.MakeCodeBundle(c.binDir, c.tarballPath)
@@ -54,18 +35,21 @@ func (c *mkBundleCmdState) Execute(ctx context.Context, fs *flag.FlagSet,
 	return subcommands.ExitSuccess
 }
 
-var mkBundleCmd = &mkBundleCmdState{
-	cmd: cmd{
-		name:      "mk-bundle",
-		shortDesc: "bundle experiment code into a tarball",
-	},
+func bundleVar(v *string, fs *flag.FlagSet) {
+	fs.StringVar(v, "bundle", "heyp-bundle.tar.xz",
+		"path to xz'd output tarball")
+}
+
+func remdirVar(v *string, fs *flag.FlagSet) {
+	fs.StringVar(v, "remdir", "heyp",
+		"root directory of deployed code/logs/etc. at remote")
 }
 
 func main() {
 	subcommands.Register(subcommands.HelpCommand(), "")
 	subcommands.Register(subcommands.FlagsCommand(), "")
 	subcommands.Register(subcommands.CommandsCommand(), "")
-	subcommands.Register(mkBundleCmd, "")
+	subcommands.Register(new(mkBundleCmd), "")
 
 	flag.Parse()
 
