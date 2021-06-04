ba988d95578b5329f3b33a69c1b47abd857e036e
Muhammed Uluyol <uluyol@google.com>
2021-06-04 01:45:49 +0000

    cmd/flowselsim: parameterize along LOPRI frac and refactor output
    The fraction of traffic that should use LOPRI is now a config
    flag as it really is a part of the workload like the number of
    hosts (and others). With this change, we can clearly see that the
    greatest different between the intended split and the actual occurs
    when the split = 0.5. (This happens because near 0 and 1, the error
    in one direction is bounded - we can't be more than 0.1 below if we
    only want to send 0.1 as LOPRI).
    
    This commit also changes the output to use reflection as prep work
    for dumping more metrics.

--- go/cmd/flowselsim/main.go
+++ go/cmd/flowselsim/main.go
@@ -10,8 +10,7 @@ import (
 )
 
 const usageText = `
-flowselsim performs a monte carlo simulation for candidate flow selection policies.
-It outputs a csv file with rows of the form NumFlows,SplitErrP05,SplitErrP50,SplitErrP95,AbsSplitErrP05,AbsSplitErrP50,AbsSplitErrP95
+flowselsim performs monte carlo simulations for different flow selection policies.
 `
 
 func usage() {
@@ -49,26 +48,59 @@ func (a *intArray) String() string {
 	return sb.String()
 }
 
+type f64Array []float64
+
+func (a *f64Array) Set(s string) error {
+	strVals := strings.Split(s, ",")
+	*a = nil
+	for _, sv := range strVals {
+		v, err := strconv.ParseFloat(sv, 64)
+		if err != nil {
+			return err
+		}
+		*a = append(*a, v)
+	}
+	return nil
+}
+
+func (a *f64Array) String() string {
+	var sb strings.Builder
+	first := true
+	for _, v := range *a {
+		if first {
+			first = false
+		} else {
+			sb.WriteByte(',')
+		}
+		sb.WriteString(strconv.FormatFloat(v, 'f', -1, 64))
+	}
+	return sb.String()
+}
+
 var _ flag.Value = new(intArray)
+var _ flag.Value = new(f64Array)
 
 func main() {
 	log.SetFlags(0)
 	log.SetPrefix("flowselsim: ")
 
 	var (
-		config   SimConfig
-		numFlows = intArray{100}
+		config         SimConfig
+		numFlows       = intArray{100}
+		fracLimitLOPRI = f64Array{0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.999999}
 	)
 	flag.Var(&config.Method, "method",
 		"method to select flows (valid values are "+strings.Join(ValidSelectionMethods(), " ")+")")
 	flag.Var(&numFlows, "flows", "csv list of total number of flows (bottlenecked + empty)")
 	flag.IntVar(&config.NumHosts, "hosts", 10, "total number of hosts")
 	flag.IntVar(&config.NumBottleneckFlows, "bottlenecked", 10, "number of bottlenecked flows")
+	flag.Var(&fracLimitLOPRI, "frac-lopri", "csv list of fraction of limit that uses LOPRI")
 	flag.Float64Var(&config.OverflowThresh, "overflow-thresh", 0.75, "for host-use-hipri-first: amount of HIPRI that must be used before overflowing to LOPRI")
 	flag.Usage = usage
 
 	flag.Parse()
 	config.NumFlows = []int(numFlows)
+	config.FracLimitLOPRI = []float64(fracLimitLOPRI)
 
 	if err := ValidateConfig(config); err != nil {
 		log.Fatal(err)
--- go/cmd/flowselsim/sim.go
+++ go/cmd/flowselsim/sim.go
@@ -10,7 +10,9 @@ import (
 	"log"
 	"math"
 	"os"
+	"reflect"
 	"sort"
+	"strconv"
 	"strings"
 	"sync"
 	"time"
@@ -54,6 +56,7 @@ type SimConfig struct {
 	NumFlows           []int
 	NumHosts           int
 	NumBottleneckFlows int
+	FracLimitLOPRI     []float64
 
 	OverflowThresh float64
 }
@@ -75,6 +78,12 @@ func ValidateConfig(c SimConfig) error {
 	}
 
 	var errs []string
+	for _, frac := range c.FracLimitLOPRI {
+		if frac < 0 || 1 <= frac {
+			errs = append(errs, "all LOPRI fracs must be in [0, 1)")
+			break
+		}
+	}
 	if minNumFlows < 1 {
 		errs = append(errs, "need at least one flow")
 	}
@@ -93,25 +102,66 @@ func ValidateConfig(c SimConfig) error {
 	return nil
 }
 
+func metrics() []string {
+	typ := reflect.TypeOf(distResults{})
+	var ret []string
+	for i := 0; i < typ.NumField(); i++ {
+		field := typ.Field(i)
+		ret = append(ret, field.Name)
+	}
+	return ret
+}
+
+func summarize(sb *strings.Builder, numFlows int, fracLOPRI float64, r distResults) {
+	val := reflect.ValueOf(r)
+	sb.WriteString(strconv.Itoa(numFlows))
+	sb.WriteByte(',')
+	sb.WriteString(strconv.FormatFloat(fracLOPRI, 'f', -1, 64))
+	for i := 0; i < val.NumField(); i++ {
+		f := val.Field(i)
+		data := f.Interface().([]float64)
+		sb.WriteByte(',')
+		sb.WriteString(strconv.FormatFloat(stat.Quantile(0.50, stat.LinInterp, data, nil), 'f', 4, 64))
+		sb.WriteByte(',')
+		sb.WriteString(strconv.FormatFloat(stat.Quantile(0.95, stat.LinInterp, data, nil), 'f', 4, 64))
+	}
+}
+
 func RunSimluation(c SimConfig) {
-	rows := make([]string, len(c.NumFlows))
+	results := make([]simRunResult, len(c.NumFlows))
 	var wg sync.WaitGroup
 	wg.Add(len(c.NumFlows))
 	for i := range c.NumFlows {
 		go func(i int) {
 			defer wg.Done()
-			rows[i] = runSim(c.NumFlows[i], c)
+			results[i] = runSim(c.NumFlows[i], c)
 		}(i)
 	}
 	wg.Wait()
 
 	out := bufio.NewWriter(os.Stdout)
 	defer out.Flush()
-	fmt.Fprintf(out, "NumFlows,SplitErrP05,SplitErrP50,SplitErrP95,AbsSplitErrP05,AbsSplitErrP50,AbsSplitErrP95\n")
-	for _, r := range rows {
-		out.WriteString(r)
+
+	out.WriteString("NumFlows,FracLOPRI")
+	for _, m := range metrics() {
+		out.WriteString(",")
+		out.WriteString(m)
+		out.WriteString("P50")
+		out.WriteString(",")
+		out.WriteString(m)
+		out.WriteString("P95")
+	}
+	out.WriteString("\n")
+
+	for _, r := range results {
+		out.WriteString(r.summaryRows)
 		out.WriteByte('\n')
 	}
+}
+
+type simRunResult struct {
+	summaryRows string
+	distRows    string
 }
 
 var debugMu sync.Mutex
@@ -170,115 +220,128 @@ func (t *trial) dump() string {
 	return string(b)
 }
 
-func runSim(numFlows int, c SimConfig) string {
-	const numTrials = 100_000
+type distResults struct {
+	SplitErr    []float64
+	AbsSplitErr []float64
+}
 
-	splitErr := make([]float64, numTrials)
-	absSplitErr := make([]float64, numTrials)
+func runSim(numFlows int, c SimConfig) simRunResult {
+	const numTrials = 1_000
+
+	results := make([]distResults, len(c.FracLimitLOPRI))
+	for i := range results {
+		results[i].SplitErr = make([]float64, numTrials)
+		results[i].AbsSplitErr = make([]float64, numTrials)
+	}
 
 	const batchSize = 500
 	var wg sync.WaitGroup
-	for ntrialStart := 0; ntrialStart < numTrials; ntrialStart += batchSize {
-		wg.Add(1)
-		go func(ntrialStart int, rng rand.Source) {
-			defer wg.Done()
-			ntrialEnd := ntrialStart + batchSize
-			if ntrialEnd > numTrials {
-				ntrialEnd = numTrials
-			}
 
-			randDist := rand.New(rng)
-			var trial trial
-			for ntrial := ntrialStart; ntrial < ntrialEnd; ntrial++ {
-				trial.reset()
-				clearIntsToLen(&trial.BottleneckedIDs, c.NumBottleneckFlows)
-				sampleuv.WithoutReplacement(trial.BottleneckedIDs, numFlows, rng)
-				clearIntsToLen(&trial.HostCutpoints, c.NumHosts+1)
-				trial.HostCutpoints[0] = 0
-				trial.HostCutpoints[c.NumHosts] = numFlows
-				if len(trial.HostCutpoints) > 2 {
-					// debugMu.Lock()
-					// println(len(trial.HostCutpoints[1:c.NumHosts]))
-					// println(numFlows - 2)
-					sampleuv.WithoutReplacement(trial.HostCutpoints[1:c.NumHosts], numFlows-1, rng)
-					// debugMu.Unlock()
+	for resultIndex := range results {
+		resultIndex := resultIndex
+		wantLOPRIFrac := c.FracLimitLOPRI[resultIndex]
+		for ntrialStart := 0; ntrialStart < numTrials; ntrialStart += batchSize {
+			wg.Add(1)
+			go func(ntrialStart int, rng rand.Source) {
+				defer wg.Done()
+				ntrialEnd := ntrialStart + batchSize
+				if ntrialEnd > numTrials {
+					ntrialEnd = numTrials
 				}
-				for i := 1; i < len(trial.HostCutpoints)-1; i++ {
-					trial.HostCutpoints[i]++
-				}
-				sort.Ints(trial.BottleneckedIDs)
-				sort.Ints(trial.HostCutpoints)
 
-				clearHostsToLen(&trial.Hosts, c.NumHosts)
+				randDist := rand.New(rng)
+				var trial trial
+				for ntrial := ntrialStart; ntrial < ntrialEnd; ntrial++ {
+					trial.reset()
+					clearIntsToLen(&trial.BottleneckedIDs, c.NumBottleneckFlows)
+					sampleuv.WithoutReplacement(trial.BottleneckedIDs, numFlows, rng)
+					clearIntsToLen(&trial.HostCutpoints, c.NumHosts+1)
+					trial.HostCutpoints[0] = 0
+					trial.HostCutpoints[c.NumHosts] = numFlows
+					if len(trial.HostCutpoints) > 2 {
+						// debugMu.Lock()
+						// println(len(trial.HostCutpoints[1:c.NumHosts]))
+						// println(numFlows - 2)
+						sampleuv.WithoutReplacement(trial.HostCutpoints[1:c.NumHosts], numFlows-1, rng)
+						// debugMu.Unlock()
+					}
+					for i := 1; i < len(trial.HostCutpoints)-1; i++ {
+						trial.HostCutpoints[i]++
+					}
+					sort.Ints(trial.BottleneckedIDs)
+					sort.Ints(trial.HostCutpoints)
 
-				for hostID := 0; hostID < c.NumHosts; hostID++ {
-					startFlowID := trial.HostCutpoints[hostID]
-					endFlowID := trial.HostCutpoints[hostID+1]
-					trial.Hosts[hostID].numFlows = endFlowID - startFlowID
+					// wantLOPRIFrac := randDist.Float64()
 
-					ids := trial.BottleneckedIDs
-					i := sort.SearchInts(ids, startFlowID)
-					for i < len(ids) && startFlowID <= ids[i] && ids[i] < endFlowID {
-						trial.Hosts[hostID].numBottlenecked++
-						i++
+					clearHostsToLen(&trial.Hosts, c.NumHosts)
+
+					for hostID := 0; hostID < c.NumHosts; hostID++ {
+						startFlowID := trial.HostCutpoints[hostID]
+						endFlowID := trial.HostCutpoints[hostID+1]
+						trial.Hosts[hostID].numFlows = endFlowID - startFlowID
+
+						ids := trial.BottleneckedIDs
+						i := sort.SearchInts(ids, startFlowID)
+						for i < len(ids) && startFlowID <= ids[i] && ids[i] < endFlowID {
+							trial.Hosts[hostID].numBottlenecked++
+							i++
+						}
+
+						// for _, flowID := range trial.BottleneckedIDs {
+						// 	if startFlowID <= flowID && flowID < endFlowID {
+						// 		trial.Hosts[hostID].numBottlenecked++
+						// 	}
+						// }
 					}
 
-					// for _, flowID := range trial.BottleneckedIDs {
-					// 	if startFlowID <= flowID && flowID < endFlowID {
-					// 		trial.Hosts[hostID].numBottlenecked++
-					// 	}
-					// }
-				}
-
-				// Double check
-				{
-					var countTotal, countBottlenecked int
-					for _, h := range trial.Hosts {
-						countTotal += h.numFlows
-						countBottlenecked += h.numBottlenecked
+					// Double check
+					{
+						var countTotal, countBottlenecked int
+						for _, h := range trial.Hosts {
+							countTotal += h.numFlows
+							countBottlenecked += h.numBottlenecked
+						}
+						if countTotal != numFlows || countBottlenecked != c.NumBottleneckFlows {
+							log.Fatalf("internal error: numFlows (have %d, want %d) numBottleneckedFlows (have %d, want %d)\n%s",
+								countTotal, numFlows, countBottlenecked, c.NumBottleneckFlows,
+								trial.dump())
+						}
 					}
-					if countTotal != numFlows || countBottlenecked != c.NumBottleneckFlows {
-						log.Fatalf("internal error: numFlows (have %d, want %d) numBottleneckedFlows (have %d, want %d)\n%s",
-							countTotal, numFlows, countBottlenecked, c.NumBottleneckFlows,
-							trial.dump())
+
+					var numBottleneckedLOPRI int
+					switch c.Method {
+					case SelRandomProb:
+						numBottleneckedLOPRI = numBottleneckedLOPRIRandomProb(&trial, wantLOPRIFrac, randDist)
+					case SelHostUseHIPRIFirst:
+						numBottleneckedLOPRI = numBottleneckedLOPRIHostUseHIPRIFirst(&trial, wantLOPRIFrac, &c, randDist)
+					default:
+						log.Fatal("selection method not implemented")
 					}
+
+					actualLOPRIFrac := float64(numBottleneckedLOPRI) /
+						float64(c.NumBottleneckFlows)
+					results[resultIndex].SplitErr[ntrial] = wantLOPRIFrac - actualLOPRIFrac
+					results[resultIndex].AbsSplitErr[ntrial] = math.Abs(results[resultIndex].SplitErr[ntrial])
 				}
-
-				wantLOPRIPct := randDist.Float64()
-
-				var numBottleneckedLOPRI int
-				switch c.Method {
-				case SelRandomProb:
-					numBottleneckedLOPRI = numBottleneckedLOPRIRandomProb(&trial, wantLOPRIPct, randDist)
-				case SelHostUseHIPRIFirst:
-					numBottleneckedLOPRI = numBottleneckedLOPRIHostUseHIPRIFirst(&trial, wantLOPRIPct, &c, randDist)
-				default:
-					log.Fatal("selection method not implemented")
-				}
-
-				actualLOPRIPct := float64(numBottleneckedLOPRI) /
-					float64(c.NumBottleneckFlows)
-				splitErr[ntrial] = wantLOPRIPct - actualLOPRIPct
-				absSplitErr[ntrial] = math.Abs(splitErr[ntrial])
-			}
-		}(ntrialStart, rand.NewSource(uint64(time.Now().UnixNano())^rand.Uint64()))
+			}(ntrialStart, rand.NewSource(uint64(time.Now().UnixNano())^rand.Uint64()))
+		}
 	}
-
 	wg.Wait()
 
-	sort.Float64s(splitErr)
-	sort.Float64s(absSplitErr)
+	sb := new(strings.Builder)
+	for i := range results {
+		sort.Float64s(results[i].SplitErr)
+		sort.Float64s(results[i].AbsSplitErr)
 
-	return fmt.Sprintf("%d,%f,%f,%f,%f,%f,%f", numFlows,
-		// pct error
-		stat.Quantile(0.05, stat.LinInterp, splitErr, nil),
-		stat.Quantile(0.50, stat.LinInterp, splitErr, nil),
-		stat.Quantile(0.95, stat.LinInterp, splitErr, nil),
-		// abs pct error
-		stat.Quantile(0.05, stat.LinInterp, absSplitErr, nil),
-		stat.Quantile(0.50, stat.LinInterp, absSplitErr, nil),
-		stat.Quantile(0.95, stat.LinInterp, absSplitErr, nil),
-	)
+		if i > 0 {
+			sb.WriteByte('\n')
+		}
+		summarize(sb, numFlows, c.FracLimitLOPRI[i], results[i])
+	}
+
+	return simRunResult{
+		summaryRows: sb.String(),
+	}
 }
 
 func numBottleneckedLOPRIRandomProb(t *trial, wantLOPRIPct float64, randDist *rand.Rand) int {
