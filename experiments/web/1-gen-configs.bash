9ece25c6772689f8c034f54257a79654eeb1b70c
Muhammed Uluyol <uluyol@umich.edu>
2021-07-16 16:07:27 -0400

    experiments/web: delete old outdir and create correct subdir

--- experiments/web/1-gen-configs.bash
+++ experiments/web/1-gen-configs.bash
@@ -9,8 +9,10 @@ fi
 
 outdir=${1%/}
 
+rm -rf "$outdir"
+
 exec bin/deploy-heyp gen-configs \
     -i config.star \
-    -o "$outdir" \
+    -o "$outdir/configs" \
     -rspec rspec.xml \
     -ssh-user uluyol
