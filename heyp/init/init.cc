3a63f1dc1c91ce21d194b4169c0440954670ac23
Muhammed Uluyol <uluyol@umich.edu>
2021-07-16 21:06:40 -0400

    init: ignore leak in argv since we retain it until shutdown

--- heyp/init/init.cc
+++ heyp/init/init.cc
@@ -3,6 +3,7 @@
 #include <cstdlib>
 
 #include "absl/debugging/failure_signal_handler.h"
+#include "absl/debugging/leak_check.h"
 #include "absl/debugging/symbolize.h"
 #include "absl/flags/parse.h"
 #include "heyp/log/logging.h"
@@ -15,7 +16,7 @@ void MainInit(int* argc, char*** argv) {
   absl::InstallFailureSignalHandler(absl::FailureSignalHandlerOptions());
   std::vector<char*> updated = absl::ParseCommandLine(*argc, *argv);
   *argc = updated.size();
-  char** new_argv = static_cast<char**>(calloc(*argc, sizeof(char*)));
+  char** new_argv = absl::IgnoreLeak(static_cast<char**>(calloc(*argc, sizeof(char*))));
   for (size_t i = 0; i < updated.size(); ++i) {
     new_argv[i] = updated[i];
   }
