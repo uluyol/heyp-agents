8b3a7d2d5c5c528379d75ca2739544d431c12620
Muhammed Uluyol <uluyol@umich.edu>
2021-08-16 13:15:56 -0400

    tools: bump envoy version

--- tools/collect-aux-bins.bash
+++ tools/collect-aux-bins.bash
@@ -5,6 +5,7 @@ set -e
 TOINSTALL=${@:-envoy fortio collect-host-stats}
 
 ENVOY_URL="https://dl.getenvoy.io/public/raw/files/getenvoy-envoy-1.18.3.p0.g98c1c9e-1p77.gb76c773-linux-glibc-release-x86_64.tar.xz"
+ENVOY_URL="https://archive.tetratelabs.io/envoy/download/v1.19.0/envoy-v1.19.0-linux-amd64.tar.xz"
 
 mkdir -p aux-bin
 
@@ -13,8 +14,8 @@ for cmd in $TOINSTALL; do
   case $cmd in
   envoy)
     wget -qO- "$ENVOY_URL" | tar xJ -C aux-bin
-    mv aux-bin/getenvoy*glibc*/bin/envoy aux-bin/envoy
-    rm -r aux-bin/getenvoy*glibc*
+    mv aux-bin/envoy*linux*/bin/envoy aux-bin/envoy
+    rm -r aux-bin/envoy*linux*
     ;;
   fortio)
     GOOS=linux GOARCH=amd64 GOBIN=$PWD/aux-bin go install fortio.org/fortio@v1.16.0
