c6f469a335d887bd16f51d8601af104417bae889
Muhammed Uluyol <uluyol@umich.edu>
2021-09-14 12:27:41 -0400

    experiments/web: plot latency CDFs too and parallelize computation

--- experiments/web/4-summarize-latency.R
+++ experiments/web/4-summarize-latency.R
@@ -1,6 +1,7 @@
 #!/usr/bin/env Rscript
 
 library(ggplot2)
+library(parallel)
 
 args <- commandArgs(trailingOnly=TRUE)
 
@@ -19,6 +20,44 @@ SYS_LONG[["stableqos"]] <- "MixedStable"
 SYS_LONG[["hipri"]] <- "AllHIPRI"
 SYS_LONG[["lopri"]] <- "AllLOPRI"
 
+PlotLatencyCDFTo <- function(subset, output) {
+    if (nrow(subset) > 0) {
+        subset$Sys <- factor(subset$Sys, levels=c("NoLimit", "AllHIPRI", "MixedStable", "MixedFlipFlop", "AllLOPRI"))
+        subset$Y <- subset$Percentile / 100
+
+        pdf(output, height=2.5, width=5)
+        p <- ggplot(data=subset, aes(x=LatencyNanos / 1e6, y=Y, color=Sys, linetype=Sys)) +
+            geom_step(size=1) +
+            xlab("Latency (ms)") +
+            ylab("CDF across time") +
+            coord_cartesian(xlim=c(0, 350), ylim=c(0, 1), expand=FALSE) +
+            scale_y_continuous(breaks=seq(0, 1, by=0.2)) +
+            theme_bw() +
+            guides(color=guide_legend(ncol=3), linetype=guide_legend(ncol=3)) +
+            theme(
+                legend.title=element_blank(),
+                legend.position="top",
+                legend.margin=margin(0, 0, 0, 0),
+                legend.box.margin=margin(-4, -4, -8, 0),
+                legend.background=element_rect(color="black", fill="white", linetype="blank", size=0),
+                legend.direction="horizontal",
+                legend.key=element_blank(),
+                legend.key.height=unit(11, "points"),
+                legend.key.width=unit(25, "points"),
+                legend.spacing.x=unit(1, "points"),
+                legend.spacing.y=unit(0, "points"),
+                legend.text=element_text(size=11, margin=margin(r=10)),
+                strip.background=element_rect(color="white", fill="white"),
+                strip.text=element_text(size=12),
+                plot.margin=unit(c(5.5, 8.5, 5.5, 5.5), "points"),
+                axis.text=element_text(color="black", size=11),
+                axis.title.y=element_text(size=12, margin=margin(0, 3, 0, 0)),
+                axis.title.x=element_text(size=12, margin=margin(3, 0, 0, 0)))
+        print(p)
+        .junk <- dev.off()
+    }
+}
+
 PlotLatencyCCDFTo <- function(subset, output) {
     if (nrow(subset) > 0) {
         subset$Sys <- factor(subset$Sys, levels=c("NoLimit", "AllHIPRI", "MixedStable", "MixedFlipFlop", "AllLOPRI"))
@@ -30,7 +69,7 @@ PlotLatencyCCDFTo <- function(subset, output) {
             geom_step(size=1) +
             xlab("Latency (ms)") +
             ylab("CCDF across time") +
-            coord_cartesian(xlim=c(0, 350), ylim=c(0.0005, 1), expand=FALSE) +
+            coord_cartesian(xlim=c(0, 450), ylim=c(0.0005, 1), expand=FALSE) +
             scale_y_log10(breaks=c(0.001, 0.01, 0.1, 0.5, 1), labels=c("99.9%", "99%", "90%", "50%", "0%")) +
             theme_bw() +
             guides(color=guide_legend(ncol=3), linetype=guide_legend(ncol=3)) +
@@ -74,7 +113,7 @@ dir.create(summarydir, recursive=TRUE)
 
 cfgGroups <- basename(unique(gsub("-[^-]+$", "", Sys.glob(file.path(outdir, "proc-indiv", "*")))))
 
-for (cfgGroup in cfgGroups) {
+ProcCfgGroup <- function(cfgGroup) {
     procdirs <- Sys.glob(file.path(outdir, "proc-indiv", paste0(cfgGroup, "*")))
 
     latency <- data.frame()
@@ -98,9 +137,15 @@ for (cfgGroup in cfgGroups) {
 
     for (group in unique(latency$Group)) {
         for (inst in unique(latency$Instance)) {
+            PlotLatencyCDFTo(
+                latency[latency$Group == group & latency$Instance == inst,],
+                file.path(summarydir, paste0(cfgGroup, "-cdf-", group, "_", inst, ".pdf")))
             PlotLatencyCCDFTo(
                 latency[latency$Group == group & latency$Instance == inst,],
                 file.path(summarydir, paste0(cfgGroup, "-ccdf-", group, "_", inst, ".pdf")))
         }
     }
 }
+
+numCores <- parallel::detectCores()
+.junk <- parallel::mclapply(cfgGroups, ProcCfgGroup, mc.cores = numCores)
