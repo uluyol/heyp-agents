b4039dd010923656446f7c28be6de933ff938ec2
Muhammed Uluyol <uluyol@umich.edu>
2021-09-21 19:23:47 -0400

    proc: add readers for some generated data

---
+++ go/proc/align_read.go
@@ -0,0 +1,47 @@
+package proc
+
+import (
+	"encoding/json"
+	"io"
+
+	"github.com/uluyol/heyp-agents/go/pb"
+	"google.golang.org/protobuf/encoding/protojson"
+)
+
+type HostAgentStatsMesg struct {
+	M *pb.InfoBundle
+}
+
+func (m *HostAgentStatsMesg) UnmarshalJSON(b []byte) error {
+	return protojson.Unmarshal(b, m.M)
+}
+
+type AlignedHostAgentStats struct {
+	UnixSec float64                       `json:"unixSec"`
+	Data    map[string]HostAgentStatsMesg `json:"data"`
+}
+
+type AlignedHostAgentStatsReader struct {
+	next AlignedHostAgentStats
+	e    error
+	dec  *json.Decoder
+}
+
+func (r *AlignedHostAgentStatsReader) Next() bool {
+	r.next = AlignedHostAgentStats{}
+	r.e = r.dec.Decode(&r.next)
+	if r.e == io.EOF {
+		r.e = nil
+		return false
+	}
+	return r.e == nil
+}
+
+func (r *AlignedHostAgentStatsReader) Err() error                 { return r.e }
+func (r *AlignedHostAgentStatsReader) Get() AlignedHostAgentStats { return r.next }
+
+func NewAlignedHostAgentStatsReader(r io.Reader) *AlignedHostAgentStatsReader {
+	return &AlignedHostAgentStatsReader{
+		dec: json.NewDecoder(r),
+	}
+}
--- go/proc/fortio.go
+++ go/proc/fortio.go
@@ -1,9 +1,13 @@
 package proc
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"io/fs"
 	"regexp"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -17,4 +21,96 @@ func GetStartEndFortio(fsys fs.FS) (time.Time, time.Time, error) {
 	}
 
 	return getStartEnd(fsys, logs)
+}
+
+type FortioDemandSnapshot struct {
+	UnixSec  float64
+	FGDemand map[string]float64
+}
+
+type FortioDemandTraceReader struct {
+	next      FortioDemandSnapshot
+	e         error
+	s         *bufio.Scanner
+	internFGs map[string]string
+
+	line       int
+	processCur bool
+	isHeader   bool
+}
+
+func (r *FortioDemandTraceReader) Next() bool {
+	r.next = FortioDemandSnapshot{UnixSec: -1}
+	if r.processCur {
+		r.processCur = false
+		if r.processLine() {
+			panic("processing leftover line cannot finalize a snapshot")
+		}
+	}
+	for r.e == nil && r.s.Scan() {
+		r.line++
+		finishedSnap := r.processLine()
+		if finishedSnap {
+			return true
+		}
+	}
+	if r.e == nil {
+		r.e = r.s.Err()
+	}
+	return r.e == nil && r.next.UnixSec != -1
+}
+
+func (r *FortioDemandTraceReader) processLine() (finishedSnap bool) {
+	if r.e != nil {
+		return false
+	}
+	if r.isHeader {
+		r.isHeader = false
+		if !strings.HasPrefix(r.s.Text(), "UnixTime,FG,Demand") {
+			r.e = fmt.Errorf("bad header: wanted UnixTime,FG,Demand to be the first 3 fields, saw %s", r.s.Text())
+		}
+		return false
+	}
+	fields := strings.Split(r.s.Text(), ",")
+	if len(fields) < 3 {
+		r.e = fmt.Errorf("line %d: wanted 3 fields, saw %s", r.line, r.s.Bytes())
+		return false
+	}
+	unixSec, err := strconv.ParseFloat(fields[0], 64)
+	if err != nil {
+		r.e = fmt.Errorf("line %d: failed to parse time, saw %s", r.line, r.s.Bytes())
+		return false
+	}
+	fg, ok := r.internFGs[fields[1]]
+	if !ok {
+		fg = fields[1]
+		r.internFGs[fg] = fg
+	}
+	demandBps, err := strconv.ParseFloat(fields[2], 64)
+	if err != nil {
+		r.e = fmt.Errorf("line %d: failed to parse demand, saw %s", r.line, r.s.Bytes())
+		return false
+	}
+	if r.next.UnixSec == -1 {
+		r.next = FortioDemandSnapshot{
+			UnixSec:  unixSec,
+			FGDemand: make(map[string]float64),
+		}
+	} else if unixSec != r.next.UnixSec {
+		r.processCur = true // need to reprocess this line
+		return true
+	}
+	r.next.FGDemand[fg] = demandBps
+	return false
+}
+
+func (r *FortioDemandTraceReader) Err() error                { return r.e }
+func (r *FortioDemandTraceReader) Get() FortioDemandSnapshot { return r.next }
+
+func NewFortioDemandTraceReader(r io.Reader) *FortioDemandTraceReader {
+	return &FortioDemandTraceReader{
+		s:         bufio.NewScanner(r),
+		internFGs: make(map[string]string),
+		isHeader:  true,
+	}
 }
