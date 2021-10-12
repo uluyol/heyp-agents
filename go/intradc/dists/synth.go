6418242b7941666a344c8d174d1da10139696004
Muhammed Uluyol <uluyol@umich.edu>
2021-10-12 14:16:51 -0400

    intradc/dists: add distribution means

--- go/intradc/dists/synth.go
+++ go/intradc/dists/synth.go
@@ -8,6 +8,7 @@ import (
 
 type DistGen interface {
 	GenDist(*rand.Rand) []float64
+	DistMean() float64 // not sample mean
 }
 
 type UniformGen struct {
@@ -16,6 +17,8 @@ type UniformGen struct {
 	High float64
 	Num  int
 }
+
+var _ DistGen = UniformGen{}
 
 func (g UniformGen) GenDist(rng *rand.Rand) []float64 {
 	d := make([]float64, g.Num)
@@ -26,14 +29,24 @@ func (g UniformGen) GenDist(rng *rand.Rand) []float64 {
 	return d
 }
 
+func (g UniformGen) DistMean() float64 { return (g.High + g.Low) / 2 }
+
 type ElephantsMiceGen struct {
 	_         struct{}
 	Elephants UniformGen
 	Mice      UniformGen
 }
 
+var _ DistGen = ElephantsMiceGen{}
+
 func (g ElephantsMiceGen) GenDist(rng *rand.Rand) []float64 {
 	return append(g.Elephants.GenDist(rng), g.Mice.GenDist(rng)...)
+}
+
+func (g ElephantsMiceGen) DistMean() float64 {
+	s := g.Elephants.DistMean() * float64(g.Elephants.Num)
+	s += g.Mice.DistMean() * float64(g.Mice.Num)
+	return s / float64(g.Elephants.Num+g.Mice.Num)
 }
 
 type ExponentialGen struct {
@@ -43,6 +56,8 @@ type ExponentialGen struct {
 	Num  int
 }
 
+var _ DistGen = ExponentialGen{}
+
 func (g ExponentialGen) GenDist(rng *rand.Rand) []float64 {
 	d := make([]float64, g.Num)
 	for i := range d {
@@ -50,3 +65,5 @@ func (g ExponentialGen) GenDist(rng *rand.Rand) []float64 {
 	}
 	return d
 }
+
+func (g ExponentialGen) DistMean() float64 { return g.Mean }
---
+++ go/intradc/dists/synth_test.go
@@ -0,0 +1,50 @@
+package dists
+
+import (
+	"testing"
+	"time"
+
+	"golang.org/x/exp/rand"
+)
+
+func checkMean(t *testing.T, g DistGen, errorFrac float64) {
+	t.Helper()
+	const numDistSamples = 100
+
+	rng := rand.New(rand.NewSource(uint64(time.Now().UnixNano())))
+	var sum, count float64
+	for i := 0; i < numDistSamples; i++ {
+		d := g.GenDist(rng)
+		count += float64(len(d))
+		for _, v := range d {
+			sum += v
+		}
+	}
+
+	sampleMean := sum / count
+	distMean := g.DistMean()
+	if sampleMean < (1-errorFrac)*distMean || (1+errorFrac)*distMean < sampleMean {
+		t.Errorf("bad sample mean: got %v want approx %v", sampleMean, distMean)
+	}
+}
+
+const typicalErrFrac = 0.025
+
+func TestUniformGen(t *testing.T) {
+	checkMean(t, UniformGen{Low: 100, High: 99999, Num: 100}, typicalErrFrac)
+	checkMean(t, UniformGen{High: 99999, Num: 100}, typicalErrFrac)
+	checkMean(t, UniformGen{High: 1, Num: 100}, typicalErrFrac)
+}
+
+func TestElephantsMiceGen(t *testing.T) {
+	checkMean(t, ElephantsMiceGen{Elephants: UniformGen{Low: 100, High: 99999, Num: 3}, Mice: UniformGen{High: 10, Num: 100}}, 0.1) // higher error because there are few elephants
+	checkMean(t, ElephantsMiceGen{Elephants: UniformGen{Low: 11200, High: 1120011, Num: 100}, Mice: UniformGen{High: 10, Num: 10}}, typicalErrFrac)
+	checkMean(t, ElephantsMiceGen{Elephants: UniformGen{Low: 11200, High: 1120011, Num: 100}, Mice: UniformGen{High: 10, Num: 0}}, typicalErrFrac)
+	checkMean(t, ElephantsMiceGen{Elephants: UniformGen{Low: 11200, High: 1120011, Num: 0}, Mice: UniformGen{High: 10, Num: 2}}, typicalErrFrac)
+	checkMean(t, ElephantsMiceGen{Elephants: UniformGen{Low: 11200, High: 1120011, Num: 0}, Mice: UniformGen{High: 10, Num: 0}}, typicalErrFrac)
+}
+
+func TestExponentialGen(t *testing.T) {
+	checkMean(t, ExponentialGen{Mean: 9999, Max: 9999999999, Num: 100}, typicalErrFrac)
+	checkMean(t, ExponentialGen{Mean: 2, Max: 999999, Num: 50}, typicalErrFrac)
+}
