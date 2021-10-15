cea727e28c584b8ed37f860c6b9c81a14e471bc9
Muhammed Uluyol <uluyol@umich.edu>
2021-10-15 17:14:43 -0400

    intradc/alloc: add functions for running max-min fair allocation across a distribution

--- go/intradc/alloc/fairness.go
+++ go/intradc/alloc/fairness.go
@@ -1,6 +1,8 @@
 package alloc
 
-import "sort"
+import (
+	"sort"
+)
 
 func MaxMinFairWaterlevel(capacity float64, demands []float64) float64 {
 	// Compute max-min fair HIPRI waterlevel
@@ -16,6 +18,43 @@ func MaxMinFairWaterlevel(capacity float64, demands []float64) float64 {
 			capacity -= ask
 		} else {
 			waterlevel += capacity / float64(numUnsatisfied)
+			break
+		}
+	}
+	return waterlevel
+}
+
+type ValCount struct {
+	Val           float64
+	ExpectedCount float64
+}
+
+// MaxMinFairWaterlevelDist is like MaxMinFairWaterlevel but takes a distribution
+// as input.
+//
+// demands is modified by the call.
+func MaxMinFairWaterlevelDist(capacity float64, demands []ValCount) float64 {
+	sort.Slice(demands, func(i, j int) bool {
+		return demands[i].Val < demands[j].Val
+	})
+	remainingCounts := make([]float64, len(demands))
+	{
+		var c float64
+		for i := len(demands) - 1; i >= 0; i-- {
+			c += demands[i].ExpectedCount
+			remainingCounts[i] = c
+		}
+	}
+	var waterlevel float64
+	for i := range demands {
+		delta := demands[i].Val - waterlevel
+		unsatisfiedCount := remainingCounts[i]
+		ask := delta * unsatisfiedCount
+		if ask <= capacity {
+			waterlevel += delta
+			capacity -= ask
+		} else {
+			waterlevel += capacity / unsatisfiedCount
 			break
 		}
 	}
--- go/intradc/alloc/fairness_test.go
+++ go/intradc/alloc/fairness_test.go
@@ -28,3 +28,67 @@ func TestMaxMinFairWaterlevel(t *testing.T) {
 		})
 	}
 }
+
+func TestMaxMinFairWaterlevelDistAB(t *testing.T) {
+	tests := []struct {
+		name     string
+		demands  []float64
+		capacity float64
+	}{
+		{"Empty", nil, 0},
+		{"AllZero", []float64{0, 0, 0}, 0},
+		{"AllSatisfied", []float64{101, 202, 333, 4}, 640},
+		{"AllVerySatisfied", []float64{101, 202, 333, 4}, 1000},
+		{"BiggestNotSatisfied", []float64{101, 202, 333, 4}, 639},
+		{"RepeatedValues", []float64{1, 1, 1, 1, 1.1, 1.1, 6, 100.5, 100.5, 100.5, 159, 164, 181, 2, 33, 4}, 500},
+		{"RepeatedValues", []float64{1, 1, 1, 1, 1.1, 1.1, 6, 100.5, 100.5, 100.5, 159, 164, 181, 2, 33, 4}, 1000},
+		{"NoneSatisfied", []float64{2, 5, 7}, 5},
+		{"HalfSatisfied", []float64{7, 20, 23, 99, 51}, 100},
+	}
+
+	for _, test := range tests {
+		test := test
+		t.Run(test.name, func(t *testing.T) {
+			want := MaxMinFairWaterlevel(test.capacity, test.demands)
+			got := MaxMinFairWaterlevelDist(test.capacity, compactIntoDist(test.demands))
+			if got != want {
+				t.Errorf("got %f, want %f", got, want)
+			}
+		})
+	}
+}
+
+func TestMaxMinFairWaterlevelDistFrac(t *testing.T) {
+	tests := []struct {
+		name     string
+		demands  []ValCount
+		capacity float64
+		want     float64
+	}{
+		{"AllSatisfied", []ValCount{{10, 1.5}, {20, 1}}, 35, 20},
+		{"BarelyUnsatisfied", []ValCount{{10, 1.5}, {20, 1}}, 34, 19},
+		{"AllVerySatisfied", []ValCount{{10, 1.5}, {20, 1}}, 100, 20},
+	}
+
+	for _, test := range tests {
+		test := test
+		t.Run(test.name, func(t *testing.T) {
+			got := MaxMinFairWaterlevelDist(test.capacity, test.demands)
+			if got != test.want {
+				t.Errorf("got %f, want %f", got, test.want)
+			}
+		})
+	}
+}
+
+func compactIntoDist(demands []float64) []ValCount {
+	saw := make(map[float64]int)
+	for _, v := range demands {
+		saw[v]++
+	}
+	dist := make([]ValCount, 0, len(demands))
+	for v, c := range saw {
+		dist = append(dist, ValCount{Val: v, ExpectedCount: float64(c)})
+	}
+	return dist
+}
