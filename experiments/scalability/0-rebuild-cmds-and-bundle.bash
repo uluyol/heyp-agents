a1012b43b57b22e7326e6c78dae37d1826532796
Muhammed Uluyol <uluyol@umich.edu>
2021-10-29 15:41:54 -0400

    experiments/scalability: optionally build just deploy/processing tools

--- experiments/scalability/0-rebuild-cmds-and-bundle.bash
+++ experiments/scalability/0-rebuild-cmds-and-bundle.bash
@@ -8,5 +8,8 @@ mkdir -p bin
 wd=$PWD
 cd ../..
 env CGO_LDFLAGS=-L$TOOLCHAIN/clang+llvm/lib go build -o "$wd/bin" ./go/cmd/...
+if [[ $1 == cmdsonly ]]; then
+    exit
+fi
 cd "$wd"
 bin/deploy-heyp mk-bundle -bin ../../bazel-bin -auxbin ../../aux-bin
