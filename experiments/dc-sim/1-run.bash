34c9558f19263a189dccf1ddff66798d5a090869
Muhammed Uluyol <uluyol@umich.edu>
2021-11-02 19:51:19 -0400

    experiments/dc-sim: save log

--- experiments/dc-sim/1-run.bash
+++ experiments/dc-sim/1-run.bash
@@ -2,7 +2,6 @@
 
 set -e
 
-
 if [[ $# -lt 1 ]]; then
   echo usage: $0 outdir >&2
   exit 1
@@ -13,5 +12,7 @@ outdir=${1%/}
 mkdir -p "$outdir"
 cp config.yaml "$outdir/config.yaml"
 bin/dc-control-sim \
-    -c "$outdir/config.yaml" \
-    -o "$outdir/sim-data.json"
+  -c "$outdir/config.yaml" \
+  -o "$outdir/sim-data.json" \
+  2>&1 |
+  tee "$outdir/sim.log"
