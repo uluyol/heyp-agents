3c9561377e480c1669dac4045f1cf0a43cbf2466
Muhammed Uluyol <uluyol@umich.edu>
2021-11-16 14:59:24 -0500

    intradc/flowsel: add hybrid flow selection
    This is an experiment. May need tweaking.

--- go/intradc/flowsel/selector.go
+++ go/intradc/flowsel/selector.go
@@ -3,6 +3,7 @@ package flowsel
 import (
 	"encoding/binary"
 	"math"
+	"sort"
 
 	xxhash "github.com/cespare/xxhash/v2"
 	"github.com/uluyol/heyp-agents/go/calg"
@@ -14,11 +15,36 @@ type SampledUsages struct {
 	HostIDs []int
 }
 
+type byUsage struct{ *SampledUsages }
+
+func (s byUsage) Len() int { return len(s.Usages) }
+
+func (s byUsage) Swap(i, j int) {
+	s.Usages[i], s.Usages[j] = s.Usages[j], s.Usages[i]
+	s.HostIDs[i], s.HostIDs[j] = s.HostIDs[j], s.HostIDs[i]
+}
+
+func (s byUsage) Less(i, j int) bool {
+	if s.Usages[i] == s.Usages[j] {
+		return s.HostIDs[i] < s.HostIDs[j]
+	}
+	return s.Usages[i] > s.Usages[j]
+}
+
+// SortByUsage sorts the sampled data in decreasing order of usage.
+func (s *SampledUsages) SortByUsage() {
+	if len(s.Usages) != len(s.HostIDs) {
+		panic("mismatched lengths")
+	}
+	sort.Sort(byUsage{s})
+}
+
 type Matcher interface {
 	MatchHosts(usages []float64) (matched []int, matchedUsage float64)
 }
 
 type Selector interface {
+	// data should be sorted in decreasing order by usage
 	NewMatcher(matchFrac float64, data SampledUsages) Matcher
 	Name() string
 }
@@ -80,3 +106,70 @@ func (KnapsackSelector) NewMatcher(matchFrac float64, data SampledUsages) Matche
 func (KnapsackSelector) Name() string { return "knapsack" }
 
 var _ Selector = KnapsackSelector{}
+
+// HybridSelector picks the top N items in a round-robin fashion, ordered with descending usage.
+// The rest are picked via hashing. The aim to combine the benefits of hashing and explicit selection.
+// TODO: memory to bound QoS churn.
+type HybridSelector struct {
+	NumRR int
+}
+
+type hybridMatcher struct {
+	preMatched, pre []int
+	thresh          uint32
+}
+
+func bsContains(data []int, x int) bool {
+	i := sort.SearchInts(data, x)
+	return i < len(data) && data[i] == x
+}
+
+func (m hybridMatcher) MatchHosts(usages []float64) ([]int, float64) {
+	matched := append([]int(nil), m.preMatched...)
+	matchedUsage := sumUsage(usages, m.preMatched)
+	for id := range usages {
+		var idBytes [4]byte
+		binary.LittleEndian.PutUint32(idBytes[:], uint32(id))
+		hashedID := sum32(idBytes[:])
+		if hashedID <= m.thresh && !bsContains(m.pre, id) {
+			matched = append(matched, id)
+			matchedUsage += usages[id]
+		}
+	}
+	return matched, matchedUsage
+}
+
+func (s HybridSelector) NewMatcher(matchFrac float64, data SampledUsages) Matcher {
+	var lopriInterval int
+	if matchFrac <= 0 {
+		lopriInterval = s.NumRR + 1
+	} else {
+		lopriInterval = int(math.Round(1 / matchFrac))
+	}
+
+	preMatched := make([]int, 0, int((matchFrac)*float64(s.NumRR)+2))
+	pre := make([]int, 0, s.NumRR)
+
+	for i := 0; i < s.NumRR && i < len(data.HostIDs); i++ {
+		id := data.HostIDs[i]
+		// Match every Nth, starting by not matching.
+		// This biases the usage to be over the target, not fall short.
+		if (i+1)%lopriInterval == 0 {
+			preMatched = append(preMatched, id)
+		}
+		pre = append(pre, id)
+	}
+
+	sort.Ints(pre)
+	sort.Ints(preMatched)
+
+	return hybridMatcher{
+		preMatched: preMatched,
+		pre:        pre,
+		thresh:     uint32(float64(math.MaxUint32) * matchFrac),
+	}
+}
+
+func (s HybridSelector) Name() string { return "hybrid" }
+
+var _ Selector = HybridSelector{}
--- go/intradc/flowsel/selector_test.go
+++ go/intradc/flowsel/selector_test.go
@@ -2,6 +2,7 @@ package flowsel
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 	"time"
 
@@ -9,9 +10,10 @@ import (
 )
 
 func TestHashSelectorFracZero(t *testing.T) {
+	usages := []float64{1, 1, 1, 1, 1, 1, 1, 1, 1}
 	matched, matchedUsage := HashSelector{}.
-		NewMatcher(0, SampledUsages{}).
-		MatchHosts([]float64{1, 1, 1, 1, 1, 1, 1, 1, 1})
+		NewMatcher(0, sampleAll(usages)).
+		MatchHosts(usages)
 
 	if len(matched) != 0 {
 		t.Errorf("matched more than zero hosts: got %v", matched)
@@ -22,9 +24,10 @@ func TestHashSelectorFracZero(t *testing.T) {
 }
 
 func TestHashSelectorFracOne(t *testing.T) {
+	usages := []float64{1, 2, 3, 4, 5, 6, 7, 8, 9}
 	matched, matchedUsage := HashSelector{}.
-		NewMatcher(1, SampledUsages{}).
-		MatchHosts([]float64{1, 2, 3, 4, 5, 6, 7, 8, 9})
+		NewMatcher(1, sampleAll(usages)).
+		MatchHosts(usages)
 
 	if got := len(matched); got != 9 {
 		t.Errorf("matched %v, wanted all hosts", matched)
@@ -51,7 +54,7 @@ func TestHashSelectorPartial(t *testing.T) {
 			}
 
 			matched, matchedUsage := HashSelector{}.
-				NewMatcher(testFrac, SampledUsages{}).
+				NewMatcher(testFrac, sampleAll(usages)).
 				MatchHosts(usages)
 
 			frac := float64(len(matched)) / float64(len(usages))
@@ -67,9 +70,10 @@ func TestHashSelectorPartial(t *testing.T) {
 }
 
 func TestKnapsackSelectorFracZero(t *testing.T) {
+	usages := []float64{1, 1, 1, 1, 1, 1, 1, 1, 1}
 	matched, matchedUsage := KnapsackSelector{}.
-		NewMatcher(0, SampledUsages{}).
-		MatchHosts([]float64{1, 1, 1, 1, 1, 1, 1, 1, 1})
+		NewMatcher(0, sampleAll(usages)).
+		MatchHosts(usages)
 
 	if len(matched) != 0 {
 		t.Errorf("matched more than zero hosts: got %v", matched)
@@ -80,9 +84,10 @@ func TestKnapsackSelectorFracZero(t *testing.T) {
 }
 
 func TestKnapsackSelectorFracOne(t *testing.T) {
+	usages := []float64{1, 2, 3, 4, 5, 6, 7, 8, 9}
 	matched, matchedUsage := KnapsackSelector{}.
-		NewMatcher(1, SampledUsages{}).
-		MatchHosts([]float64{1, 2, 3, 4, 5, 6, 7, 8, 9})
+		NewMatcher(1, sampleAll(usages)).
+		MatchHosts(usages)
 
 	if got := len(matched); got != 9 {
 		t.Errorf("matched %v, wanted all hosts", matched)
@@ -109,7 +114,7 @@ func TestKnapsackSelectorPartial(t *testing.T) {
 			}
 
 			matched, matchedUsage := HashSelector{}.
-				NewMatcher(testFrac, SampledUsages{}).
+				NewMatcher(testFrac, sampleAll(usages)).
 				MatchHosts(usages)
 
 			if len(matched) == 0 {
@@ -127,4 +132,157 @@ func TestKnapsackSelectorPartial(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestHybridSelectorFracZero(t *testing.T) {
+	runTest := func(t *testing.T, s HybridSelector) {
+		usages := []float64{1, 1, 1, 1, 1, 1, 1, 1, 1}
+		matched, matchedUsage := s.NewMatcher(0, sampleAll(usages)).
+			MatchHosts(usages)
+
+		if len(matched) != 0 {
+			t.Errorf("matched more than zero hosts: got %v", matched)
+		}
+		if matchedUsage != 0 {
+			t.Errorf("got matched usage = %g > 0", matchedUsage)
+		}
+	}
+
+	t.Run("NumRR=0", func(t *testing.T) { runTest(t, HybridSelector{NumRR: 0}) })
+	t.Run("NumRR=3", func(t *testing.T) { runTest(t, HybridSelector{NumRR: 3}) })
+	t.Run("NumRR=100", func(t *testing.T) { runTest(t, HybridSelector{NumRR: 100}) })
+}
+
+func TestHybridSelectorFracOne(t *testing.T) {
+	runTest := func(t *testing.T, s HybridSelector) {
+		usages := []float64{1, 2, 3, 4, 5, 6, 7, 8, 9}
+		matched, matchedUsage := s.NewMatcher(1, sampleAll(usages)).
+			MatchHosts(usages)
+
+		if got := len(matched); got != 9 {
+			t.Errorf("matched %v, wanted all hosts", matched)
+		}
+		if matchedUsage != 45 {
+			t.Errorf("got matched usage = %g != 45", matchedUsage)
+		}
+	}
+
+	t.Run("NumRR=0", func(t *testing.T) { runTest(t, HybridSelector{NumRR: 0}) })
+	t.Run("NumRR=3", func(t *testing.T) { runTest(t, HybridSelector{NumRR: 3}) })
+	t.Run("NumRR=100", func(t *testing.T) { runTest(t, HybridSelector{NumRR: 100}) })
+}
+
+func TestHybridSelectorPartial(t *testing.T) {
+	runTest := func(t *testing.T, s HybridSelector) {
+		t.Parallel()
+
+		testFracs := []float64{0.1, 0.5, 0.9}
+
+		for _, testFrac := range testFracs {
+			testFrac := testFrac
+			t.Run(fmt.Sprintf("Frac=%f", testFrac), func(t *testing.T) {
+				t.Parallel()
+				rng := rand.New(rand.NewSource(uint64(time.Now().UnixNano())))
+				usages := make([]float64, 5000)
+
+				for i := range usages {
+					usages[i] = rng.Float64() * 5000
+				}
+
+				matched, matchedUsage := s.NewMatcher(testFrac, sampleAll(usages)).
+					MatchHosts(usages)
+
+				frac := float64(len(matched)) / float64(len(usages))
+				if frac < testFrac-0.025 || testFrac+0.025 < frac {
+					t.Errorf("downgraded %d (frac = %f) of hosts", len(matched), frac)
+				}
+
+				if want := sumUsage(usages, matched); matchedUsage != want {
+					t.Errorf("miscounted usage: got %f want %f", matchedUsage, want)
+				}
+			})
+		}
+	}
+
+	t.Run("NumRR=0", func(t *testing.T) { runTest(t, HybridSelector{NumRR: 0}) })
+	t.Run("NumRR=3", func(t *testing.T) { runTest(t, HybridSelector{NumRR: 3}) })
+	t.Run("NumRR=100", func(t *testing.T) { runTest(t, HybridSelector{NumRR: 100}) })
+}
+
+func TestHybridSelectorPartialAllRR(t *testing.T) {
+	usages := []float64{1, 1, 1, 1, 1, 1, 1, 1, 1}
+	matched, matchedUsage := HybridSelector{NumRR: 100}.
+		NewMatcher(0.5, sampleAll(usages)).
+		MatchHosts(usages)
+
+	want := []int{1, 3, 5, 7}
+	if !reflect.DeepEqual(want, matched) {
+		t.Errorf("did not match expected hosts: got %v, want %v", matched, want)
+	}
+	if matchedUsage != 4 {
+		t.Errorf("got matched usage = %g != 4", matchedUsage)
+	}
+}
+
+func TestHybridSelectorPartialAllRR_DiffUsages(t *testing.T) {
+	usages := []float64{2, 1, 4, 3, 6, 5, 8, 7, 9}
+	matched, matchedUsage := HybridSelector{NumRR: 100}.
+		NewMatcher(0.5, sampleAll(usages)).
+		MatchHosts(usages)
+
+	want := []int{0, 2, 4, 6}
+	if !reflect.DeepEqual(want, matched) {
+		matchedUsages := make([]float64, len(matched))
+		for i, id := range matched {
+			matchedUsages[i] = usages[id]
+		}
+		t.Logf("matched usages = %v of %d", matchedUsages, len(usages))
+		t.Errorf("did not match expected hosts: got %v, want %v", matched, want)
+	}
+	if matchedUsage != 20 {
+		t.Errorf("got matched usage = %g != 20", matchedUsage)
+	}
+}
+
+func TestHybridSelectorPartialAllRR_DiffUsages_SmallSample(t *testing.T) {
+	usages := []float64{2, 1, 4, 3, 6, 5, 8, 7, 9}
+
+	su := SampledUsages{
+		Usages:  []float64{4, 5, 8, 7},
+		HostIDs: []int{2, 5, 6, 7},
+	}
+	su.SortByUsage()
+
+	matched, matchedUsage := HybridSelector{NumRR: 100}.
+		NewMatcher(0.5, su).
+		MatchHosts(usages)
+
+	want := []int{
+		// via fixed selection
+		2, 7,
+		// via hashing (just ran and copied results)
+		0, 8,
+	}
+	if !reflect.DeepEqual(want, matched) {
+		matchedUsages := make([]float64, len(matched))
+		for i, id := range matched {
+			matchedUsages[i] = usages[id]
+		}
+		t.Logf("matched usages = %v of %d", matchedUsages, len(usages))
+		t.Errorf("did not match expected hosts: got %v, want %v", matched, want)
+	}
+	if matchedUsage != 22 {
+		t.Errorf("got matched usage = %g != 22", matchedUsage)
+	}
+}
+
+func sampleAll(usages []float64) SampledUsages {
+	u := append([]float64(nil), usages...)
+	ids := make([]int, len(u))
+	for i := range u {
+		ids[i] = i
+	}
+	su := SampledUsages{Usages: u, HostIDs: ids}
+	su.SortByUsage()
+	return su
 }
