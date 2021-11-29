5d1dbb9e885bb31c24bce3cb86faa334c5791e72
Muhammed Uluyol <uluyol@umich.edu>
2021-11-28 19:37:32 -0500

    experiments/dc-sim: optionally set number of runs

--- experiments/dc-sim/1-run.bash
+++ experiments/dc-sim/1-run.bash
@@ -9,11 +9,13 @@ fi
 
 outdir=${1%/}
 config=${CONFIG:-config.yaml}
+runs=${RUNS:-100}
 
 mkdir -p "$outdir"
 cp "$config" "$outdir/config.yaml"
 bin/dc-control-sim \
   -c "$outdir/config.yaml" \
   -o "$outdir/sim-data.json" \
+  -runs "$runs" \
   2>&1 |
   tee "$outdir/sim.log"
