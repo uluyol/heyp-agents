9b5edd4bbf2acc8c20766add520f443fb4e1a72b
Muhammed Uluyol <uluyol@umich.edu>
2022-01-16 15:25:27 -0500

    experiments/feedback-sim: monte carlo simulations of feedback control

---
+++ experiments/feedback-sim/1-run.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+if [[ $# -lt 1 ]]; then
+  echo usage: $0 outdir >&2
+  exit 1
+fi
+
+outdir=${1%/}
+config=${CONFIG:-config.yaml}
+runs=${RUNS:-100}
+
+mkdir -p "$outdir"
+cp "$config" "$outdir/config.yaml"
+bin/dc-control-sim \
+  feedback-sim \
+  -c "$outdir/config.yaml" \
+  -o "$outdir/sim-data.json" \
+  -runs "$runs" \
+  2>&1 |
+  tee "$outdir/sim.log"
