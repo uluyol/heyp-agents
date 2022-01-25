19a936bf9dd90a430537bb99e48e7e39f0cdedd8
Muhammed Uluyol <uluyol@umich.edu>
2022-01-25 01:02:48 -0500

    experiments/web: remove unused plot - we have too many anyway

--- experiments/web/code/plot-fg-usage-ts.R
+++ experiments/web/code/plot-fg-usage-ts.R
@@ -131,43 +131,7 @@ PlotFGNode <- function(fg, output) {
   .junk <- dev.off()
 }
 
-PlotAgg <- function(output) {
-  t <- aggregate(Usage ~ FG + Timestamp, FUN=sum, data=data)
-
-  pdf(output, height=2.5, width=5)
-  p <- ggplot(t, aes(x=Timestamp, y=Usage, color=FG)) +
-      geom_line(size=1) +
-      xlab("Time (sec)") +
-      ylab("Usage (Gbps)") +
-      coord_cartesian(ylim=c(0, 0)) +
-      scale_y_continuous(breaks=seq(0, 16, by=2)) +
-      theme_bw() +
-      guides(color=guide_legend(ncol=2)) +
-      theme(
-          legend.title=element_blank(),
-          legend.position="top",
-          legend.margin=margin(0, 0, 0, 0),
-          legend.box.margin=margin(-4, -4, -8, 0),
-          legend.background=element_rect(color="black", fill="white", linetype="blank", size=0),
-          legend.direction="horizontal",
-          legend.key=element_blank(),
-          legend.key.height=unit(11, "points"),
-          legend.key.width=unit(25, "points"),
-          legend.spacing.x=unit(1, "points"),
-          legend.spacing.y=unit(0, "points"),
-          legend.text=element_text(size=11, margin=margin(r=10)),
-          strip.background=element_rect(color="white", fill="white"),
-          strip.text=element_text(size=12),
-          plot.margin=unit(c(5.5, 8.5, 5.5, 5.5), "points"),
-          axis.text=element_text(color="black", size=11),
-          axis.title.y=element_text(size=12, margin=margin(0, 3, 0, 0)),
-          axis.title.x=element_text(size=12, margin=margin(3, 0, 0, 0)))
-  print(p)
-  .junk <- dev.off()
-}
-
-tasks <- list(
-  parallel::mcparallel(PlotAgg(paste0(outpre, "usage-ts-agg.pdf"))))
+tasks <- list()
 
 for (fg in fgs) {
   tasks <- append(tasks, parallel::mcparallel(PlotFG(fg, "Usage", "Usage", paste0(outpre, "usage-ts-fg-", fg, ".pdf"))))
