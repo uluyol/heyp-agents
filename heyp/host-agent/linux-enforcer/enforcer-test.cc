4cc5fbec95fd317cd66e2d0cb0a15298b639738f
Muhammed Uluyol <uluyol@umich.edu>
2022-01-27 00:25:43 -0500

    host-agent/linux-enforcer: reduce calls to tc
    Until now, we've been changing the host configuration in 3 steps:
    first increase any rate limits, then flip QoS rules, and finish by
    decreasing rate limits.
    
    However, we only really care to change rate limits if that
    particular HTB class is being used by some flow. If we see that no
    flows are matched, we can safely ignore the change in rate limit.
    This matters particularly with our current usage where each host is
    given just 1 QoS, and when switching QoS we reducing the rate limit
    for the unused QoS to 0.
    
    This commit implements this optimization and performs better in
    this case: we no longer change the rate limit for the unused class
    and a combined change of QoS+limit is 1 call to tc + 1 call to
    iptables. Changing just the rate limit or QoS is even better, we
    only call one of the tools.

--- heyp/host-agent/linux-enforcer/enforcer-test.cc
+++ heyp/host-agent/linux-enforcer/enforcer-test.cc
@@ -271,8 +271,7 @@ TEST(LinuxHostEnforcer, UnchangedAllocs) {
   auto ipt_runner = std::make_unique<MockIptRunner>();
 
   std::string change_rl_tc =
-      "class change dev eth1 parent 1: classid 1:2 htb rate 100.000000mbit\n"
-      "class change dev eth1 parent 1: classid 1:3 htb rate 5.000000mbit\n";
+      "class change dev eth1 parent 1: classid 1:2 htb rate 100.000000mbit\n";
 
   auto& tc_exp1 =
       EXPECT_CALL(*tc_caller, Batch(absl::Cord(WantInitWanTc()), true)).Times(1);
@@ -311,8 +310,7 @@ TEST(LinuxHostEnforcer, ChangedAllocsOnly) {
   auto ipt_runner = std::make_unique<MockIptRunner>();
 
   std::string change_rl_tc1 =
-      "class change dev eth1 parent 1: classid 1:2 htb rate 100.000000mbit\n"
-      "class change dev eth1 parent 1: classid 1:3 htb rate 5.000000mbit\n";
+      "class change dev eth1 parent 1: classid 1:2 htb rate 100.000000mbit\n";
   std::string change_rl_tc2 =
       "class change dev eth1 parent 1: classid 1:2 htb rate 200.000000mbit\n";
 
@@ -362,14 +360,15 @@ TEST(LinuxHostEnforcer, ChangedAllocsAndPri) {
   auto ipt_runner = std::make_unique<MockIptRunner>();
 
   std::string change_rl_tc1 =
-      "class change dev eth1 parent 1: classid 1:2 htb rate 100.000000mbit\n"
-      "class change dev eth1 parent 1: classid 1:3 htb rate 5.000000mbit\n";
+      "class change dev eth1 parent 1: classid 1:2 htb rate 100.000000mbit\n";
   std::string change_rl_tc2 =
       "class change dev eth1 parent 1: classid 1:3 htb rate 200.000000mbit\n";
   std::string change_rl_tc3 =
-      "class change dev eth1 parent 1: classid 1:2 htb rate 5.000000mbit\n";
+      "class change dev eth1 parent 1: classid 1:2 htb rate 500.000000mbit\n";
+  std::string change_rl_tc4 =
+      "class change dev eth1 parent 1: classid 1:3 htb rate 50.000000mbit\n";
 
-  std::string change_ipt =
+  std::string change_ipt1 =
       "*mangle\n"
       "-D OUTPUT -o eth1 -p tcp -m tcp -d 10.0.0.2 -j CLASSIFY --set-class 1:2\n"
       "-D FORWARD -o eth1 -p tcp -m tcp -d 10.0.0.2 -j CLASSIFY --set-class 1:2\n"
@@ -397,6 +396,34 @@ TEST(LinuxHostEnforcer, ChangedAllocsAndPri) {
       "-A FORWARD -o eth1 -p tcp -m tcp -d 10.0.0.3 -j RETURN\n"
       "COMMIT\n";
 
+  std::string change_ipt2 =
+      "*mangle\n"
+      "-D OUTPUT -o eth1 -p tcp -m tcp -d 10.0.0.2 -j CLASSIFY --set-class 1:3\n"
+      "-D FORWARD -o eth1 -p tcp -m tcp -d 10.0.0.2 -j CLASSIFY --set-class 1:3\n"
+      "-D OUTPUT -o eth1 -p tcp -m tcp -d 10.0.0.2 -j DSCP --set-dscp-class BE\n"
+      "-D FORWARD -o eth1 -p tcp -m tcp -d 10.0.0.2 -j DSCP --set-dscp-class BE\n"
+      "-D OUTPUT -o eth1 -p tcp -m tcp -d 10.0.0.2 -j RETURN\n"
+      "-D FORWARD -o eth1 -p tcp -m tcp -d 10.0.0.2 -j RETURN\n"
+      "-D OUTPUT -o eth1 -p tcp -m tcp -d 10.0.0.3 -j CLASSIFY --set-class 1:3\n"
+      "-D FORWARD -o eth1 -p tcp -m tcp -d 10.0.0.3 -j CLASSIFY --set-class 1:3\n"
+      "-D OUTPUT -o eth1 -p tcp -m tcp -d 10.0.0.3 -j DSCP --set-dscp-class BE\n"
+      "-D FORWARD -o eth1 -p tcp -m tcp -d 10.0.0.3 -j DSCP --set-dscp-class BE\n"
+      "-D OUTPUT -o eth1 -p tcp -m tcp -d 10.0.0.3 -j RETURN\n"
+      "-D FORWARD -o eth1 -p tcp -m tcp -d 10.0.0.3 -j RETURN\n"
+      "-A OUTPUT -o eth1 -p tcp -m tcp -d 10.0.0.2 -j CLASSIFY --set-class 1:2\n"
+      "-A FORWARD -o eth1 -p tcp -m tcp -d 10.0.0.2 -j CLASSIFY --set-class 1:2\n"
+      "-A OUTPUT -o eth1 -p tcp -m tcp -d 10.0.0.2 -j DSCP --set-dscp-class AF21\n"
+      "-A FORWARD -o eth1 -p tcp -m tcp -d 10.0.0.2 -j DSCP --set-dscp-class AF21\n"
+      "-A OUTPUT -o eth1 -p tcp -m tcp -d 10.0.0.2 -j RETURN\n"
+      "-A FORWARD -o eth1 -p tcp -m tcp -d 10.0.0.2 -j RETURN\n"
+      "-A OUTPUT -o eth1 -p tcp -m tcp -d 10.0.0.3 -j CLASSIFY --set-class 1:2\n"
+      "-A FORWARD -o eth1 -p tcp -m tcp -d 10.0.0.3 -j CLASSIFY --set-class 1:2\n"
+      "-A OUTPUT -o eth1 -p tcp -m tcp -d 10.0.0.3 -j DSCP --set-dscp-class AF21\n"
+      "-A FORWARD -o eth1 -p tcp -m tcp -d 10.0.0.3 -j DSCP --set-dscp-class AF21\n"
+      "-A OUTPUT -o eth1 -p tcp -m tcp -d 10.0.0.3 -j RETURN\n"
+      "-A FORWARD -o eth1 -p tcp -m tcp -d 10.0.0.3 -j RETURN\n"
+      "COMMIT\n";
+
   auto& tc_exp1 =
       EXPECT_CALL(*tc_caller, Batch(absl::Cord(WantInitWanTc()), true)).Times(1);
   auto& tc_exp2 = EXPECT_CALL(*tc_caller, Batch(absl::Cord(change_rl_tc1), true))
@@ -405,7 +432,10 @@ TEST(LinuxHostEnforcer, ChangedAllocsAndPri) {
   auto& tc_exp3 = EXPECT_CALL(*tc_caller, Batch(absl::Cord(change_rl_tc2), true))
                       .Times(1)
                       .After(tc_exp2);
-  EXPECT_CALL(*tc_caller, Batch(absl::Cord(change_rl_tc3), true)).Times(1).After(tc_exp3);
+  auto& tc_exp4 = EXPECT_CALL(*tc_caller, Batch(absl::Cord(change_rl_tc3), true))
+                      .Times(1)
+                      .After(tc_exp3);
+  EXPECT_CALL(*tc_caller, Batch(absl::Cord(change_rl_tc4), true)).Times(1).After(tc_exp4);
   EXPECT_CALL(*tc_caller, Call(_, _)).Times(2);
   EXPECT_CALL(*tc_caller, RawOut()).Times(0);
   EXPECT_CALL(*tc_caller, GetResult()).Times(0);
@@ -421,11 +451,23 @@ TEST(LinuxHostEnforcer, ChangedAllocsAndPri) {
                                                  .restore_counters = false}))
           .Times(1)
           .After(ipt_exp1);
-  EXPECT_CALL(*ipt_runner, Restore(iptables::Table::kMangle, absl::Cord(change_ipt),
+  auto& ipt_exp3 =
+      EXPECT_CALL(*ipt_runner, Restore(iptables::Table::kMangle, absl::Cord(change_ipt1),
+                                       iptables::RestoreFlags{.flush_tables = false,
+                                                              .restore_counters = false}))
+          .Times(1)
+          .After(ipt_exp2);
+  auto& ipt_exp4 =
+      EXPECT_CALL(*ipt_runner, Restore(iptables::Table::kMangle, absl::Cord(change_ipt2),
+                                       iptables::RestoreFlags{.flush_tables = false,
+                                                              .restore_counters = false}))
+          .Times(1)
+          .After(ipt_exp3);
+  EXPECT_CALL(*ipt_runner, Restore(iptables::Table::kMangle, absl::Cord(change_ipt1),
                                    iptables::RestoreFlags{.flush_tables = false,
                                                           .restore_counters = false}))
       .Times(1)
-      .After(ipt_exp2);
+      .After(ipt_exp4);
 
   proto::AllocBundle allocs1 = ParseTextProto<proto::AllocBundle>(R"(
     flow_allocs {
@@ -439,6 +481,18 @@ TEST(LinuxHostEnforcer, ChangedAllocsAndPri) {
       lopri_rate_limit_bps: 209715200
     }
   )");
+  proto::AllocBundle allocs3 = ParseTextProto<proto::AllocBundle>(R"(
+    flow_allocs {
+      flow { src_dc: "A" dst_dc: "B" }
+      hipri_rate_limit_bps: 524288000
+    }
+  )");
+  proto::AllocBundle allocs4 = ParseTextProto<proto::AllocBundle>(R"(
+    flow_allocs {
+      flow { src_dc: "A" dst_dc: "B" }
+      lopri_rate_limit_bps: 52428800
+    }
+  )");
 
   EnforcerTestState state = MakeEnforcerTestState(
       proto::HostEnforcerConfig(), std::move(tc_caller), std::move(ipt_runner));
@@ -446,19 +500,15 @@ TEST(LinuxHostEnforcer, ChangedAllocsAndPri) {
   EXPECT_TRUE(state.enforcer->InitSimulatedWan(state.netem_config, true).ok());
   state.enforcer->EnforceAllocs(MockFlowStateProvider(), allocs1);
   state.enforcer->EnforceAllocs(MockFlowStateProvider(), allocs2);
+  state.enforcer->EnforceAllocs(MockFlowStateProvider(), allocs3);
+  state.enforcer->EnforceAllocs(MockFlowStateProvider(), allocs4);
 }
 
 TEST(LinuxHostEnforcer, ChangedButIngoredHIPRIAllocs) {
   auto tc_caller = std::make_unique<MockTcCaller>();
   auto ipt_runner = std::make_unique<MockIptRunner>();
 
-  // only LOPRI changes
-  std::string change_rl_tc1 =
-      "class change dev eth1 parent 1: classid 1:3 htb rate 5.000000mbit\n";
-
-  auto& tc_exp1 =
-      EXPECT_CALL(*tc_caller, Batch(absl::Cord(WantInitWanTc()), true)).Times(1);
-  EXPECT_CALL(*tc_caller, Batch(absl::Cord(change_rl_tc1), true)).Times(1).After(tc_exp1);
+  EXPECT_CALL(*tc_caller, Batch(absl::Cord(WantInitWanTc()), true)).Times(1);
   EXPECT_CALL(*tc_caller, Call(_, _)).Times(2);
   EXPECT_CALL(*tc_caller, RawOut()).Times(0);
   EXPECT_CALL(*tc_caller, GetResult()).Times(0);
@@ -500,10 +550,6 @@ TEST(LinuxHostEnforcer, ChangedButIngoredLOPRIAllocs) {
   auto tc_caller = std::make_unique<MockTcCaller>();
   auto ipt_runner = std::make_unique<MockIptRunner>();
 
-  // only HIPRI changes
-  std::string change_rl_tc1 =
-      "class change dev eth1 parent 1: classid 1:2 htb rate 5.000000mbit\n";
-
   std::string change_ipt =
       "*mangle\n"
       "-D OUTPUT -o eth1 -p tcp -m tcp -d 10.0.0.2 -j CLASSIFY --set-class 1:2\n"
@@ -532,9 +578,7 @@ TEST(LinuxHostEnforcer, ChangedButIngoredLOPRIAllocs) {
       "-A FORWARD -o eth1 -p tcp -m tcp -d 10.0.0.3 -j RETURN\n"
       "COMMIT\n";
 
-  auto& tc_exp1 =
-      EXPECT_CALL(*tc_caller, Batch(absl::Cord(WantInitWanTc()), true)).Times(1);
-  EXPECT_CALL(*tc_caller, Batch(absl::Cord(change_rl_tc1), true)).Times(1).After(tc_exp1);
+  EXPECT_CALL(*tc_caller, Batch(absl::Cord(WantInitWanTc()), true)).Times(1);
   EXPECT_CALL(*tc_caller, Call(_, _)).Times(2);
   EXPECT_CALL(*tc_caller, RawOut()).Times(0);
   EXPECT_CALL(*tc_caller, GetResult()).Times(0);
