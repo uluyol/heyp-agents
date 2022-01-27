69285c90b0aa462f7581db20b4cea2ed73dbb6db
goelayu <goelayu@umich.edu>
2022-01-27 18:08:54 -0500

    fixing host agent sampling

--- go/deploy/actions/hostsim.go
+++ go/deploy/actions/hostsim.go
@@ -106,13 +106,14 @@ func RunHostAgentSims(c *pb.DeploymentConfig, remoteTopdir string, showOut bool)
 		c := &simConfigs[i]
 
 		fakeFGs := make([]*pb.FakeFG, len(c.C.FakeFgs))
+		hostsPerNode := (int(c.C.GetNumHostsPerFg()) + len(c.Nodes) - 1) / len(c.Nodes)
 		for j := range fakeFGs {
 			fakeFGs[j] = &pb.FakeFG{
 				SrcDc:            &c.SrcDC,
 				DstDc:            c.C.FakeFgs[j].DstDc,
 				Job:              c.C.FakeFgs[j].Job,
-				MinHostUsage:     proto.Int64(c.C.FakeFgs[j].GetMinFgUsage() / int64(len(c.Nodes))),
-				MaxHostUsage:     proto.Int64(c.C.FakeFgs[j].GetMaxFgUsage() / int64(len(c.Nodes))),
+				MinHostUsage:     proto.Int64(c.C.FakeFgs[j].GetMinFgUsage() / int64(c.C.GetNumHostsPerFg())),
+				MaxHostUsage:     proto.Int64(c.C.FakeFgs[j].GetMaxFgUsage() / int64(c.C.GetNumHostsPerFg())),
 				ApprovalBps:      proto.Int64(c.C.FakeFgs[j].GetApprovalBps()),
 				TargetNumSamples: proto.Int32(c.C.FakeFgs[j].GetTargetNumSamples()),
 			}
@@ -120,49 +121,54 @@ func RunHostAgentSims(c *pb.DeploymentConfig, remoteTopdir string, showOut bool)
 
 		const startHostID = 100001
 		numHosts := int32(0)
-		hostsPerNode := (int(c.C.GetNumHostsPerFg()) + len(c.Nodes) - 1) / len(c.Nodes)
+		nProcesses := 10
+		
 		for nidx, n := range c.Nodes {
 			n := n
-
-			hostSimConfig := &pb.HostSimulatorConfig{
-				Fgs:       fakeFGs,
-				Hosts:     make([]*pb.FakeHost, 0, hostsPerNode),
-				ReportDur: c.C.ReportDur,
-			}
-			for numMyHosts := 0; numMyHosts < hostsPerNode; numMyHosts++ {
-				if numHosts >= c.C.GetNumHostsPerFg() {
-					break
-				}
-				hostSimConfig.Hosts = append(hostSimConfig.Hosts, &pb.FakeHost{
-					HostId: proto.Uint64(uint64(numHosts) + startHostID),
-					FgIds:  []int32{-1},
-				})
-				numHosts++
-			}
-
-			hostSimConfigBytes, err := prototext.MarshalOptions{Indent: "  "}.Marshal(hostSimConfig)
-			if err != nil {
-				return fmt.Errorf("failed to marshal host-agent-sim config: %w", err)
-			}
-
+			hostsPerProcess := hostsPerNode/nProcesses
 			clusterAgentAddr := c.ClusterAgentAddrs[nidx%len(c.ClusterAgentAddrs)]
-			eg.Go(func() error {
-				cmd := TracingCommand(
-					LogWithPrefix("run-host-agent-sims: "),
-					"ssh", n.GetExternalAddr(),
-					fmt.Sprintf("cat > %[1]s/configs/host-agent-sim.textproto && mkdir -p %[1]s/logs && "+
-						"%[1]s/aux/host-agent-sim -c %[1]s/configs/host-agent-sim.textproto -o %[1]s/logs/host-agent-sim.csv -cluster-agent-addr %[2]s -start-time %[3]s -dur %[4]s 2>&1 | tee %[1]s/logs/host-agent-sim.log; exit ${PIPESTATUS[0]}",
-						remoteTopdir, clusterAgentAddr, startTimestamp, c.C.GetRunDur()))
-				cmd.SetStdin("host-agent-sim.textproto", bytes.NewReader(hostSimConfigBytes))
-				if showOut {
-					cmd.Stdout = os.Stdout
+			for p:= 0; p < nProcesses; p++ {
+
+
+				hostSimConfig := &pb.HostSimulatorConfig{
+					Fgs:       fakeFGs,
+					Hosts:     make([]*pb.FakeHost, 0, hostsPerProcess),
+					ReportDur: c.C.ReportDur,
 				}
-				err := cmd.Run()
+				for numMyHosts := 0; numMyHosts < hostsPerProcess; numMyHosts++ {
+					if numHosts >= c.C.GetNumHostsPerFg() {
+						break
+					}
+					hostSimConfig.Hosts = append(hostSimConfig.Hosts, &pb.FakeHost{
+						HostId: proto.Uint64(uint64(numHosts) + startHostID),
+						FgIds:  []int32{-1},
+					})
+					numHosts++
+				}
+
+				hostSimConfigBytes, err := prototext.MarshalOptions{Indent: "  "}.Marshal(hostSimConfig)
 				if err != nil {
-					return fmt.Errorf("cluster %q host agent simulator on Node %q failed: %w", c.SrcDC, n.GetName(), err)
+					return fmt.Errorf("failed to marshal host-agent-sim config: %w", err)
 				}
-				return nil
-			})
+
+				eg.Go(func() error {
+					cmd := TracingCommand(
+						LogWithPrefix("run-host-agent-sims: "),
+						"ssh", n.GetExternalAddr(),
+						fmt.Sprintf("cat > %[1]s/configs/host-agent-sim-%[5]d.textproto && mkdir -p %[1]s/logs && ulimit -Sn unlimited && "+
+							"%[1]s/aux/host-agent-sim -m %[1]s/configs/host-agent-sim-%[5]d.mutex.out -c %[1]s/configs/host-agent-sim-%[5]d.textproto -o %[1]s/logs/host-agent-sim-%[5]d.csv -cluster-agent-addr %[2]s -dur %[4]s 2>&1 | tee %[1]s/logs/host-agent-sim-%[5]d.log; exit ${PIPESTATUS[0]}",
+							remoteTopdir, clusterAgentAddr, startTimestamp, c.C.GetRunDur(),p))
+					cmd.SetStdin(fmt.Sprintf("host-agent-sim-%.textproto",p), bytes.NewReader(hostSimConfigBytes))
+					if showOut {
+						cmd.Stdout = os.Stdout
+					}
+					err := cmd.Run()
+					if err != nil {
+						return fmt.Errorf("cluster %q host agent simulator on Node %q failed: %w", c.SrcDC, n.GetName(), err)
+					}
+					return nil
+				})
+			}
 
 		}
 	}
